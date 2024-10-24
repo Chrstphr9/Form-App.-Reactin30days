2326b1ad962dfe96c67c0ce5d646c885cd30a7cd
somto <somto565@gmail.com>
2024-10-24 08:26:45 +0100

    Refactored App component to use state object and array of input objects, replaced useRef with useState, and updated FormInput components to use new state and input objects.

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,60 @@ import './App.css'
 import FormInput from "./components/FormInput"
 
 const App = () => {
-  // const [username, setUsername] = useState("")
-  const usernameRef = useRef()
+  const [value, setValue] = useState({
+    username:"",
+    email:"",
+    birthday:"",
+    passowrd:"",
+    confirmPassword:""
+  });
 
-  console.log(usernameRef);
+  const inputs = [
+    {
+      id:1,
+      name:"username",
+      type:"text",
+      placeholder:"username",
+      label:"username"
+    },
+    {
+      id:1,
+      name:"username",
+      type:"text",
+      placeholder:"username",
+      label:"username"
+    },
+    {
+      id:1,
+      name:"birthday",
+      type:"text",
+      placeholder:"birthday",
+      label:"birthday"
+    },
+    {
+      id:1,
+      name:"password",
+      type:"text",
+      placeholder:"passowrd",
+      label:"password"
+    },
+    {
+      id:1,
+      name:"confirmPassword",
+      type:"text",
+      placeholder:"confirmPassword",
+      label:"confirmPassword"
+    },
+  ]
 
   const handleSubmit = (e) => {
-
+    e.preventDefault();
   }
   
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
-        <FormInput refer={usernameRef  } placeholder="Username" />
+        <FormInput name="username" placeholder="Username" />
         <FormInput placeholder="Email" />
         <FormInput placeholder="Full Name" />
         <FormInput placeholder="Last Name" />
